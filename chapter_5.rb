96d114b582d226ab4d2191595616ed43a81a9aa0
Aric Wu <srichsun@gmail.com>
2020-06-01 09:46:00 +0800

    add scheme

---
+++ chapter_5.rb
@@ -0,0 +1,41 @@
+# chapter 5 condition
+
+# 除了false nil其他所有物件，都是true
+
+# if
+# > < =
+# empty?
+# =~
+
+if condition
+
+end
+
+if condition
+
+else
+
+end
+
+# unless
+unless condition
+
+end
+
+# case
+a = "David"
+case a
+when condition
+end
+
+# if else VS case
+
+# 會回傳true false的方法加上?
+
+def is_boy?
+
+end
+
+# ===
+/zz/ === "zzsdf"
+String === "kkk"
---
+++ chapter_6.rb
@@ -0,0 +1,26 @@
+# chapter 6 loop
+
+# times
+3.times
+
+5.times do
+
+end
+
+5.times do |i|
+
+end
+
+5.times {}
+
+# for
+
+# while
+
+# until
+
+# each
+
+# loop
+
+# break next redo
